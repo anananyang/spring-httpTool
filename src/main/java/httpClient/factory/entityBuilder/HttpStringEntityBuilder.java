c72dc9d34c692abb87d580f1fa784ed360c953be
anyang.zhu <anyang_zhu@126.com>
2021-03-02 18:54:56 +0800

    String entity

--- src/main/java/httpClient/factory/entityBuilder/HttpStringEntityBuilder.java
+++ src/main/java/httpClient/factory/entityBuilder/HttpStringEntityBuilder.java
@@ -1,8 +1,12 @@
 package httpClient.factory.entityBuilder;
 
 import com.alibaba.fastjson.JSON;
+import httpClient.constants.HttpContentType;
+import httpClient.constants.HttpHeader;
 import httpClient.request.HttpRequestConfig;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.entity.StringEntity;
 
 import java.io.UnsupportedEncodingException;
@@ -16,16 +20,27 @@ public class HttpStringEntityBuilder extends HttpEntityBuilder {
     @Override
     public HttpEntity build() throws UnsupportedEncodingException{
         Object reqBody = httpRequestConfig.getReqBody();
+        String content = this.getContent(reqBody);
+        StringEntity stringEntity = new StringEntity(content);
+        return stringEntity;
+    }
+
+    private String getContent(Object reqBody) {
         if (reqBody == null) {
             return null;
+        } else if (reqBody instanceof String) {
+            return (String) reqBody;
         }
-        /**
-         * TODO 原则上，这里要根据 Content-Type 来进行相应的处理
-         * 1. 将 Content-Type 解析成 cntentType 对象，cntentType 有 type、subType，charset, boundary
-         *     1.1、根据 subType 来判断内容来进行处理
-         */
-        String bodyStr = reqBody instanceof String ? (String) reqBody : JSON.toJSONString(reqBody);
-        StringEntity stringEntity = new StringEntity(bodyStr);
-        return stringEntity;
+
+        String contentType = httpRequestConfig.getHeaderValue(HttpHeader.CONTENT_TYPE);
+        if(StringUtils.isBlank(contentType) || !isApplicationJson(contentType)) {
+            return reqBody.toString();
+        }
+
+        return JSON.toJSONString(reqBody);
+    }
+
+    private Boolean isApplicationJson(String contentType) {
+        return contentType.indexOf(HttpContentType.APPLICATION_JSON) >= 0;
     }
 }
